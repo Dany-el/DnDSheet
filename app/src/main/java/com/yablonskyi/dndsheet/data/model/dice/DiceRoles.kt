d304d08b766a81021ad5111e9b5295942e7f670e
Daniel <daniilyablonskiy88@gmail.com>
2026-06-01 10:50:42 +0300

    add new companion object to get hitDices

--- app/src/main/java/com/yablonskyi/dndsheet/data/model/dice/DiceRoles.kt
+++ app/src/main/java/com/yablonskyi/dndsheet/data/model/dice/DiceRoles.kt
@@ -7,5 +7,10 @@ enum class DiceRoles(val roll: String) {
     D10("1d10"),
     D8("1d8"),
     D6("1d6"),
-    D4("1d4")
+    D4("1d4");
+
+    companion object {
+        val hitDices: List<String> =
+            entries.filterNot { it == D20 || it == D100 || it == D4 }.map { it.name.lowercase() }
+    }
 }
