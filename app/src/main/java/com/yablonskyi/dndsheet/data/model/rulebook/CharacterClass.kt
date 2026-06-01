2b55f4ed0ce5bb78493d2e16e9f59956a53f3ca1
Daniel <daniilyablonskiy88@gmail.com>
2026-06-01 10:40:06 +0300

    new default value for hitDice variable

--- app/src/main/java/com/yablonskyi/dndsheet/data/model/rulebook/CharacterClass.kt
+++ app/src/main/java/com/yablonskyi/dndsheet/data/model/rulebook/CharacterClass.kt
@@ -4,12 +4,15 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 import com.yablonskyi.dndsheet.data.model.character.Ability
 import com.yablonskyi.dndsheet.data.model.character.Skill
+import com.yablonskyi.dndsheet.data.model.dice.DiceRoles
+import kotlinx.serialization.Serializable
 
+@Serializable
 @Entity(tableName = "classes")
 data class CharacterClass(
     @PrimaryKey val id: String,
     val name: String,
-    val hitDice: String = "d8",
+    val hitDice: String = DiceRoles.hitDices.first(),
     val primaryAbility: Ability = Ability.STR,
     val savingThrows: Set<Ability> = emptySet(),
     val skillChoiceCount: Int = 2,
