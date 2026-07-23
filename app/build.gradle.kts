bad85fe7bc15ab3f1050a9fbe269b1bf8add37c9
Daniel <daniilyablonskiy88@gmail.com>
2026-07-24 00:03:16 +0300

    update version to v0.1.6-beta

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.yablonskyi.dndsheet"
         minSdk = 28
         targetSdk = 37
-        versionCode = 5
-        versionName = "0.1.5-beta"
+        versionCode = 6
+        versionName = "0.1.6-beta"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
